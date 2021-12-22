76a95c5b2900d7053c5d3ce140e0bfa126dca438
Sandip Rijal <sandurijal03@hotmail.com>
2021-12-22 14:16:16 +0545

    added strict mode of react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import {render } from 'react-dom'
-import Routing from './Routing'
+import React from 'react';
+import { render } from 'react-dom';
+import Routing from './Routing';
 
-
-const root = document.getElementById("root");
-render(<Routing />, root)
+const root = document.getElementById('root');
+render(
+  <React.StrictMode>
+    <Routing />
+  </React.StrictMode>,
+  root,
+);
