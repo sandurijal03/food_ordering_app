21dfe1316cde5b44f6ecdc93ba8e53aeabc5874f
Sandip Rijal <sandurijal03@hotmail.com>
2021-12-22 13:15:59 +0545

    imported pages for routing

--- src/Routing.js
+++ src/Routing.js
@@ -1,18 +1,23 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Contact from './pages/Contact';
 
 const Routing = () => {
-    return(
-        <BrowserRouter>
-        <Navbar />
-            <Routes>
-                <Route path="/" element={<Home /> } />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='menu' element={<Menu />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Routing
-
+export default Routing;
