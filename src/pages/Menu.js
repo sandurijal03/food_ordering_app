a7caa22ea3336987f09691f3b0c2268e94361617
Sandip Rijal <sandurijal03@hotmail.com>
2021-12-22 15:49:54 +0545

    menu component imported and required props passed

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,8 +1,53 @@
+import styled from 'styled-components';
+import MenuItem from '../components/MenuItem';
+import menuList from '../data';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const MenuTitle = styled.h1`
+  font-size: 4rem;
+`;
+
+const MenuList = styled.div`
+  width: 70vw;
+  height: auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  place-items: center;
+
+  @media only screen and (max-width: 1300px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media only screen and (max-width: 800px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
 const Menu = () => {
   return (
-    <div>
-      <h1>Menu</h1>
-    </div>
+    <Wrapper>
+      <MenuTitle>Our Menu</MenuTitle>
+      <MenuList>
+        {menuList.map((menuItem, key) => {
+          return (
+            <MenuItem
+              key={key}
+              name={menuItem.name}
+              image={menuItem.image}
+              price={menuItem.price}
+            />
+          );
+        })}
+      </MenuList>
+    </Wrapper>
   );
 };
 
